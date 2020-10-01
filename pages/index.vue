717de59704b5102c7ee9bd093056d9e8cf540bd0
ana-paula-oliveira97 <66676056+ana-paula-oliveira97@users.noreply.github.com>
2020-10-01 08:19:33 -0300

    ...

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">fatec-si</h1>
+      <h1 class="title">fatec-si 2020</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
